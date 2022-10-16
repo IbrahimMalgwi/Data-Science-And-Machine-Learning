cb3a96866bbbbe64b1a186fcf4112151c4b3270d
IbrahimGanaMalgwi <108299271+IbrahimMalgwi@users.noreply.github.com>
2022-10-16 02:12:37 +0100

    update to code

---
+++ project1/operation_on_array.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+var1 = np.linspace(10, 50, 100)
+# print(var1)
+
+var_array = np.array([(2, 4, 6), (1, 3, 5), (7, 24, 21), (34, 78, 90)])
+# slicing
+# print(var_array[1:, 1])
+
+# finding max
+# print(var_array.max())
+
+# finding min
+# print(var_array.min())
+
+# calculate sum
+print(var_array.sum())
