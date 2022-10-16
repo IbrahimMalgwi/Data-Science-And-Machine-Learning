568a976cf7d86cc2304c58fa506b70c438914df0
IbrahimGanaMalgwi <108299271+IbrahimMalgwi@users.noreply.github.com>
2022-10-16 02:24:58 +0100

    update to code

---
+++ project1/bassic_matrix_operation.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+array1 = np.array([(2, 4, 6), (1, 3, 5)])
+array2 = np.array([(5, 9, 3), (6, 7, 15)])
+
+# addition
+# print(array1 + array2)
+
+# multiplication
+# print(array1 * array2)
+
+# division
+# print(array1 / array2)
+
+# print on one line
+# print(array1)
+# print(array1.ravel())
+
+# sum of axi
+# print(array1)
+# print(array1.sum(axis=0))
+# print(array1.sum(axis=1))
+
+# square root
+print(np.sqrt(array1))
+
+# standard deviation
+print(np.std(array1))
