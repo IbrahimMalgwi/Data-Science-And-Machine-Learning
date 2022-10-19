dd03063584f0f2cb4243310a59c698c8c19d5eff
IbrahimGanaMalgwi <108299271+IbrahimMalgwi@users.noreply.github.com>
2022-10-19 03:41:29 +0100

    update to code

--- matplotlip_practice/matplot_graph.py
+++ matplotlip_practice/matplot_graph.py
@@ -1,5 +1,19 @@
 import matplotlib.pyplot as plt
+from matplotlib import style
 
-plt.plot([1, 3, 5, 10], [5, 10, 25, 125])
+style.use('bmh')
+
+x = [2, 4, 8]
+y = [4, 8, 24]
+
+x2 = [3, 6, 9]
+y2 = [16, 15, 30]
+
+plt.plot(x, y)
+plt.plot(x2, y2)
+
+plt.title('Test')
+plt.xlabel('Text x values')
+plt.ylabel('Text y values')
 
 plt.show()
